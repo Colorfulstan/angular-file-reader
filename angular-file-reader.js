82aa9133f4ef4dd0af3799854dd4edd9d8927a6e
krispin <jonas@krispin.it>
2016-06-13 15:36:48 +0200

    feat (methods) add missing FileReader methods

--- angular-file-reader.js
+++ angular-file-reader.js
@@ -1,29 +1,73 @@
+'use strict';
 angular.module('tw.services.fileReader', []);
 
-angular.module('tw.services.fileReader').factory('twFileReader', ['$q', '$window', '$rootScope', function($q, $window, $rootScope) {
-  var FileReader = $window.FileReader;
+angular.module('tw.services.fileReader').factory('twFileReader', ['$q', '$window', '$rootScope', function ($q, $window, $rootScope) {
+	var FileReader = $window.FileReader;
 
-  return {
-    readAsDataURL: function readAsDataURL(file, scope) {
-      var scope = scope || $rootScope;
+	return {
+		/**
+		 * @param fileOrBlob
+		 * @param [scope] default: $rootScope
+		 * @returns {Promise<string>}
+		 */
+		readAsDataURL: function readAsDataURL(fileOrBlob, scope) {
+			return read('AsDataURL', fileOrBlob, scope);
+		},
+		/**
+		 * @param fileOrBlob
+		 * @param [scope] default: $rootScope
+		 * @returns {Promise<Array>}
+		 */
+		readAsArrayBuffer: function (fileOrBlob, scope) {
+			return read('AsArrayBuffer', fileOrBlob, scope);
+		},
+		/**
+		 * @param fileOrBlob
+		 * @param [scope] default: $rootScope
+		 * @param [encoding]
+		 * @returns {Promise<string>}
+		 */
+		readAsText: function (fileOrBlob, scope, encoding) {
+			return read('AsText', fileOrBlob, scope, encoding);
+		}
+	};
 
-      return $q(function(resolve, reject) {
-        var reader = new FileReader();
+	/**
+	 *
+	 * @param method 'AsArrayBuffer' | 'AsDataURL' | 'AsText'
+	 * @param fileOrBlob
+	 * @param [scope]
+	 * @param [encoding] only used when method = 'AsText'
+	 * @returns {Promise<string | Array>} Array
+	 */
+	function read(method, fileOrBlob, scope, encoding) {
+		scope = scope || $rootScope;
 
-        reader.onload = function onload() {
-          scope.$apply(function() {
-            resolve(reader.result);
-          });
-        };
+		// NOTE: AsBinaryString is deprecated / non-standard. AsArrayBuffer should be used instead
+		var validMethods = ['AsArrayBuffer', 'AsDataURL', 'AsText'];
+		if (validMethods.indexOf(method) === -1) { throw new TypeError('Invalid method given: ' + method + ' valid methods are: ' + validMethods.toString());}
 
-        reader.onerror = function onerror() {
-          scope.$apply(function() {
-            reject(reader.error);
-          });
-        };
+		var methodName = 'read' + method;
+		return $q(function (resolve, reject) {
+			var reader = new FileReader();
 
-        reader.readAsDataURL(file);
-      });
-    }
-  };
+			reader.onload = function onload() {
+				scope.$apply(function () {
+					resolve(reader.result);
+				});
+			};
+
+			reader.onerror = function onerror() {
+				scope.$apply(function () {
+					reject(reader.error);
+				});
+			};
+
+			if (method === 'AsText'){
+				reader[methodName](fileOrBlob, encoding);
+			} else {
+				reader[methodName](fileOrBlob);
+			}
+		});
+	}
 }]);
